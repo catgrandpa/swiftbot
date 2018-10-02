81ab7f0da8ae30be2491bf74e59639d1ba6b6a70
catgrandpa <39468736+catgrandpa@users.noreply.github.com>
2018-10-02 12:48:48 -0400

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,12 +2,12 @@ var videoEl = document.querySelector('.phil-swift-damage .phil-swift-damage--con
 if (videoEl == null) {
 	var div = document.createElement('div');
 	document.body.appendChild(div);
-	div.className = 'shia-do-it';
+	div.className = 'phil-swift-damage';
 	div.style.cssText = ('position: fixed; bottom: 0px; z-index: 99999; right: 0px; height: 530px; width: 720px; pointer-events: none;');
-	div.innerHTML = '<div class="shia-do-it--container" style="width: 720px; overflow:hidden; display:block; height: 535px;"><video width="960" height="540" style="margin-top: -5px; max-width: none; padding: 0;" name="media"><source type="video/mov"></video></div>';
+	div.innerHTML = '<div class="phil-swift-damage--container" style="width: 720px; overflow:hidden; display:block; height: 535px;"><video width="960" height="540" style="margin-top: -5px; max-width: none; padding: 0;" name="media"><source type="video/webm"></video></div>';
 	var video = document.querySelector('.phil-swift-damage .phil-swift-damage--container video');
 	video.style.visibility = "hidden";
-	video.src = chrome.extension.getURL("/media/phillyswiftsdamagemontage.mov");
+	video.src = chrome.extension.getURL("/media/phillyswiftsdamagemontage.webm");
 	video.load();
 	video.addEventListener('loadeddata', function() {
 	   video.style.visibility = "visible";
